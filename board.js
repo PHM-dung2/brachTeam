af5bb9c2122f4fe795d468d88bbf5585a775b68d
PHM-dung2 <pp4992@naver.com>
2024-12-12 17:28:31 +0900

    -

--- board.js
+++ board.js
@@ -1,29 +1,27 @@
-let allArr = ["게시물1 제목 , 게시물1 내용 , 1234 , 2024-11-25 , 3",
-    "게시물2 제목 , 게시물2 내용 , 5678 , 2024-11-26 , 2",
-    "게시물3 제목 , 게시물3 내용 , 1478 , 2024-11-27 , 0"];
+let postArray = ["게시물1 제목 , 게시물1 내용 , 1234 , 2024-11-25 , 3",
+                "게시물2 제목 , 게시물2 내용 , 5678 , 2024-11-26 , 2",
+                "게시물3 제목 , 게시물3 내용 , 1478 , 2024-11-27 , 0"];
 
-// 출력함수(어디에(tbody) / 무엇을(allArr요소를 HTML로 구성해서) / 출력(.innerHTML))
-outFunc();  // 페이지가 최초로 열렸을 때 1번 실행, 등록/삭제/수정 시 실행
-function outFunc(){21
-    console.log('outFunc');
-
-    let tbody = document.querySelector('table > tbody');    //어디에
-    let html = ``;                                          //무엇을
-    for(let index = 0 ; index <= allArr.length -1; index++){
-        // index는 0 부터 마지막 인덱스까지 1씩 증가
-
-        let board = allArr[index];
-
-        let info = board.split(','); 
-        // 문자열.split => CSV형식으로 구성된 문자열을 쉼표기준으로 분해
-        //                -> 분해된 결과를 배열 원소에 저장해 반환
-        // info[0]=title, info[1]=contents, info[2]=password, info[3]=nowDate, info[4]=view
+// [3] 출력함수 , 실행조건 : js열렸을 때 최초 1번 실행 , 등록/삭제/수정 처리 성공시 실행
+postListFunc();
+function postListFunc(){  console.log('등록함수 실행')
+    // (1) 어디에 , table > tbody , document.querySelector(선택자)
+    let tbody = document.querySelector( 'table > tbody' )
+    // (2) 무엇읏 , 배열 요소들의 정보를 html 구성해서
+    html = '';
+    for( i = 0 ; i < postArray.length ; i++){
+        // index는 0부터 마지막인덱스까지 1씩 증가 반복
+        let board = postArray[i]; // 하나의 게시물
+        // 하나의 게시물을 csv 구성 했기 때문에 게시물 정보 분해 
+        // 문자열.split('기준문자') : 문자열내 기준문자로 분해해서 분해
+        let info = board.split(',') // csv 형식은 ,(쉼표)
         html += `<tr>
-                    <td>${info[3]}</td>
-                    <td> <a href="#" onclick="pageFunc(${index})"> ${info[0]}</a> </td> 
-                    <td>${info[4]}</td>
-                 </tr>` // 클릭 시 상세페이지로 전환하기 위해 제목에 하이퍼링크 지정
-    };
-   
-    tbody.innerHTML = html;                                 // 출력
-};
+                    <td>${ info[3] }</td>
+                    <td><a href="#" onclick="postTitleInput(${i})">${ info[0] }<a></td>
+                    <td>${ info[4] }</td>
+                </tr>`
+        console.log( html )
+    } // for end
+    // (3) 출력 , .innerHTML
+    tbody.innerHTML = html;
+} // f end
